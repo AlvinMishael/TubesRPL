3fb3faf27427beb34ef7c84b835a5d141bf47227
5thPhantom <6182001047@student.unpar.ac.id>
2022-12-07 12:35:30 +0700

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author keannen renaldo halim, neil christopher, wilson
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Nilai> kumpulanNilai = new ArrayList<>();
+        while (true) {
+            String namaFile = sc.next(); // path ke file nilai
+            if(namaFile.equals("0")){//jika diisi dengan 0, artinya file nya sudah habis
+                break;
+            }
+            try {
+                Nilai n1 = new Nilai();
+                FileInputStream fis = new FileInputStream(namaFile);
+                Scanner sc1 = new Scanner(fis);
+                while (sc1.hasNextLine()) {//scan isi dari file per baris
+                    String row = sc1.nextLine();
+                    n1.inputData(row);//masukkan ke dalam object Nilai
+                }
+                sc1.close();
+                kumpulanNilai.add(n1);//masukkan nilai ke dalam kumpulanNilai
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        int banyak = kumpulanNilai.size();
+        double total = 0;
+        for(int i = 0; i < kumpulanNilai.size(); i++){
+            total+=kumpulanNilai.get(i).getTotal();//cari total semua nilai
+        }
+        
+        System.out.println(total/banyak);//hitung rata rata dari semua nilai
+    }
+}
