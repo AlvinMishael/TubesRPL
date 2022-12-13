7b0abd3b4713bc911eb5d2945154f785613f65b6
Alexander Bleuvito Fevrier <6182001033@student.unpar.ac.id>
2022-12-14 00:42:20 +0700

    sebagian junit

--- test/MainTest.java
+++ test/MainTest.java
@@ -14,12 +14,61 @@ public class MainTest {
 
     @Test
     public void t01() {
-        System.out.println("hitungNilai");
-        String[] namaFile = new String[] {"C:\\Users\\vitof\\OneDrive\\Documents\\GitHub\\TubesRPL\\src"};
+        System.out.println("t01");
+        String[] namaFile = new String[] {"D:\\whitebox\\t01.txt", "0"};
         Main instance = new Main();
         double expResult = 84;
         double result = instance.hitungNilai(namaFile);
         assertEquals(expResult, result, 0);
     }
     
+    @Test
+    public void t02() {
+        System.out.println("t02");
+        String[] namaFile = new String[] {"D:\\whitebox\\t02.txt", "0"};
+        Main instance = new Main();
+        double expResult = 84;
+        double result = instance.hitungNilai(namaFile);
+        assertEquals(expResult, result, 0);
+    }
+    
+    @Test
+    public void t03() {
+        System.out.println("t03");
+        String[] namaFile = new String[] {"D:\\whitebox\\t03.txt", "0"};
+        Main instance = new Main();
+        double expResult = 84;
+        double result = instance.hitungNilai(namaFile);
+        assertEquals(expResult, result, 0);
+    }
+    
+    @Test
+    public void t04() {
+        System.out.println("t04");
+        String[] namaFile = new String[] {"D:\\whitebox\\t04.txt", "0"};
+        Main instance = new Main();
+        double expResult = 79.9;
+        double result = instance.hitungNilai(namaFile);
+        assertEquals(expResult, result, 0);
+    }
+    
+    @Test
+    public void t05() {
+        System.out.println("t05");
+        String[] namaFile = new String[] {"D:\\whitebox\\t05.txt", "0"};
+        Main instance = new Main();
+        double expResult = 100;
+        double result = instance.hitungNilai(namaFile);
+        assertNotEquals(expResult, result);
+    }
+    
+    @Test
+    public void t06() {
+        System.out.println("t06");
+        String[] namaFile = new String[] {"D:\\whitebox\\t06.txt", "0"};
+        Main instance = new Main();
+        double expResult = 99.9;
+        double result = instance.hitungNilai(namaFile);
+        assertEquals(expResult, result, 0);
+    }
 }
